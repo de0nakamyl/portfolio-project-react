cb2bce9ec56790e8d06708cce3b0b1fd1054326b
Deona Pineda <deonapineda@gmail.com>
2022-08-25 19:33:53 -0700

    CampsiteDetailPage with useParams

--- Desktop/NucampFolder/3-React/nucampsite/src/App.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage />}
+          />
       </Routes>
       <Footer />
     </div>
