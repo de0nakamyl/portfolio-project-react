25866e8fe426996ed4173505f2ba94768ae11334
Deona Pineda <deonapineda@gmail.com>
2022-08-30 20:55:13 -0700

    AnimatedDisplayCard

--- Desktop/NucampFolder/3-React/nucampsite/src/App.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='about' element={<AboutPage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
         <Route
